9f341d7c410290c9fbe92479eb98d348522a3481
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-09 18:19:49 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,8 @@
 # Use this space to code along with the readme
 
 phrase = "Hello World!"
+
+'puts phrase
 puts phrase
 puts phrase
 puts phrase
@@ -9,5 +11,20 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-puts phrase
-puts phrase
+puts phrase'
+
+def greeting
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+greeting
