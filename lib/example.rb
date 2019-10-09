b5ef20f0967ee69e963e019040570fcac5579c20
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-09 18:11:58 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,8 @@
 # Use this space to code along with the readme
 
 phrase = "Hello World!"
+
+'puts phrase
 puts phrase
 puts phrase
 puts phrase
@@ -9,8 +11,7 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-puts phrase
-puts phrase
+puts phrase'
 
 def greeting
   puts phrase
