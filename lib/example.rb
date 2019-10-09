8dfa31357781e0a5fba4469550d6baa390cc4b5f
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-09 18:10:25 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,16 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def greeting
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
